f606eb7b23b577dc09c61699d68db9f636b85d04
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-08-04 13:14:32 +0300

    task_4

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@
 
 const colors = ['red', 'green', 'blue'];
 
-console.log(colors.length);
-
+console.log(colors.length); 
 
 
 
@@ -15,3 +14,26 @@ console.log(colors.length);
 const animals = ['monkey', 'dog', 'cat']
 
 console.log(animals[animals.length - 1]); 
+
+
+
+// Task 3
+/* const numbers = [5, 43, 63, 23, 90];
+console.log(numbers.splice(0,4));   */
+
+
+// Task 4
+
+const students = ['Polina', 'Dasha', 'Masha'];
+
+/// последний элемент
+students.pop(2);
+students.push('Borya');
+
+
+/// первый элемент
+students.shift();
+students.unshift('Andrey');
+
+
+console.log(students);
